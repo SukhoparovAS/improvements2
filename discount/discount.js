a2fdcd268fc86da540e301b66d07892fd7d1eea1
sukhoparovas <sukhoparov.aleksandr@mail.ru>
2023-05-20 14:02:54 +0300

    11111

--- discount/discount.js
+++ discount/discount.js
@@ -1,14 +1,20 @@
 const discountPush = $('.discount-push')
 const activateBtn = discountPush.find('.discount-push__btn')
-const timer = $('.discount-push-timer')
+const timer2 = $('.discount-push-timer')
 const discountText = $('.discount-push__text')
 
-const startTime = 10 //время обратного отсчета в время в часах
+let startTime = 10 * 60 * 60 //время обратного отсчета в время в секундах
+let activateTime = Cookies.get('saler_timer');
+let serverTime = document.getElementById("server_time").innerHTML;
+
+if (activateTime){
+    startTime  = startTime -  (serverTime - activateTime)
+}
 
 // Устанавливаем время обратного отсчета в микросекундах 
-var countDownDate = new Date().getTime() + (startTime * 60 * 60 * 1000);
+var countDownDate = new Date().getTime() + (startTime * 1000);
 
-function startTimer() {
+function startTimer1() {
     var x = setInterval(function () {
 
         // Получаем текущее время
@@ -16,25 +22,31 @@ function startTimer() {
 
         // Вычисляем время, оставшееся до конечной даты
         var distance = countDownDate - now;
+        if (distance > 0) {
+             // Вычисляем часы, минуты и секунды
+            var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+            var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+            var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-        // Вычисляем часы, минуты и секунды
-        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-        // Обновляем отображение таймера
-        $(".timer-hours").text(hours < 10 ? "0" + hours : hours)
-        $(".timer-minutes").text(minutes < 10 ? "0" + minutes : minutes)
-        $(".timer-seconds").text(seconds < 10 ? "0" + seconds : seconds)
+            // Обновляем отображение таймера
+            timer2.find('.d-timer-hours').text(hours < 10 ? "0" + hours : hours)
+            timer2.find(".d-timer-minutes").text(minutes < 10 ? "0" + minutes : minutes)
+            timer2.find(".d-timer-seconds").text(seconds < 10 ? "0" + seconds : seconds)
+        } else {
+            clearInterval(x)
+        }
+       
     }, 1000)
 }
 
 function activateDiscount() {
     discountPush.addClass('discount-push_activate')
     discountText.text('discount activated')
-    timer.removeClass('hidden')
-    startTimer()
+    timer2.removeClass('hidden')
+    startTimer1()
 }
 
-
+if (activateTime){
+    activateDiscount()
+}
 activateBtn.click(activateDiscount)
